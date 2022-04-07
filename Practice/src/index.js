bdae037ccd8c8fab740575f8517c0157fab41e2b
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-07 15:08:56 +0530

    day2

--- Practice/src/index.js
+++ Practice/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
 const h1 = React.createElement("h1", { className: "redText" }, "Hello React!");
 
+const input = React.createElement("input", { id: "data" });
+const btn = React.createElement("button", { id: "btn" }, "Click me!");
+
+const card = React.createElement("div", { id: "card" }, h1, input, btn);
+
+
+
 ReactDOM.render(
-    h1,
+    card,
     document.getElementById("root")
 );
 
