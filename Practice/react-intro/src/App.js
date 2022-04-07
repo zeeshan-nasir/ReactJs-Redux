d631990c01bd970a7d452a356f287a81a2144bf4
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-07 16:58:06 +0530

    day2

--- Practice/react-intro/src/App.js
+++ Practice/react-intro/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const food = ["pizza", "burger", "dosa", "zinger"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="redText">Hello React</h1>
+        <input id="data" />
+        <button id="btn">Click Me!</button>
+      </div>
+      <div>
+        {food.map(e => {
+          return <Foodtype name={e} />
+        })}
+      </div>
     </div>
   );
 }
+
+const Foodtype = (props) => {
+  return <p>My favourite food is {props.name} </p>;
+}
+
+
+// function App() {
+//   const food = 'Pizza 🍕';
+//   const age = 17;
+//   const name = 'Adnan';
+//   const games = ['Dota 2', 'CS:GO', 'PUBG', 'PUBGM', 'A'];
+//   return (
+//     <div className="App">
+//       <h3>
+//         My name is {name}. My age is {age}, and my favourite food is {food}. I
+//         am {age > 18 ? 'eligible' : 'ineligible'} for license.
+//       </h3>
+//       <div>
+//         {games.map((e) => {
+//           return <Games key={e} name={e} />;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// const Games = (props) => {
+//   // console.log(props.name)
+//   return <p key={props.name}>{props.name}</p>;
+// };
 
 export default App;
