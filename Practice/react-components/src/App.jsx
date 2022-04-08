bdac9dec11adf0834ac72a1ad4dd8a58e14ee521
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-08 16:43:43 +0530

    fixed

--- Practice/react-components/src/App.jsx
+++ Practice/react-components/src/App.jsx
@@ -4,16 +4,16 @@ import './App.css';
 
 
 function App() {
-   const navbar = [
-      {
-         image: "https://rukminim2.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100",
-         title: "Top Offers"
-      },
-      {
-         image: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
-         title: "Grocery"
-      }
-   ]
+   // const navbar = [
+   //    {
+   //       image: "https://rukminim2.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100",
+   //       title: "Top Offers"
+   //    },
+   //    {
+   //       image: "https://rukminim2.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+   //       title: "Grocery"
+   //    }
+   // ]
 
    const [counter, setCounter] = useState(0);
 
