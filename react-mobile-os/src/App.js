38b11531e7f92df493429fb79fba1f1de159e0e1
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-08 17:00:41 +0530

    fixed

--- react-mobile-os/src/App.js
+++ react-mobile-os/src/App.js
@@ -1,30 +1,34 @@
 import './App.css';
 
 function App() {
-  const os = ["Android", "Blackberry", "iPhone", "Windows Phone"];
-  const company = ["Samsung", "HTC", "Micromax", "Apple"];
+   const os = ["Android", "Blackberry", "iPhone", "Windows Phone"];
+   const company = [["Samsung", "square"], ["HTC", "square"], ["Micromax", "disc"], ["Apple", "circle"]];
 
-  return (
-    <div className="App">
-      <Heading title="Mobile Operating System" />
-      {os.map(e => {
-        return <List name={e} />
-      })}
+   return (
+      <div className="App">
+         <Heading title="Mobile Operating System" />
+         {os.map(e => {
+            return <List name={e} />
+         })}
 
-      <Heading title="Mobile Manufacturers" />
-      {company.map(e => {
-        return <List name={e} />
-      })}
-    </div>
-  );
+         <Heading title="Mobile Manufacturers" />
+         {company.map(e => {
+            return <ListMarker name={e[0]} type={e[1]} />
+         })}
+      </div>
+   );
 }
 
 const Heading = (props) => {
-  return <h1>{props.title}</h1>
+   return <h1>{props.title}</h1>
 }
 
 const List = (props) => {
-  return <li className="list">{props.name}</li>
+   return <li className="list">{props.name}</li>
+}
+
+const ListMarker = (props) => {
+   return <li className={props.type}>{props.name}</li>
 }
 
 export default App;
