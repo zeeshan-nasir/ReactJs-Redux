222635cdf800fa434a525d303406d5bf50ab8167
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-09 23:13:39 +0530

    final

--- react-counter/src/components/Counter.jsx
+++ react-counter/src/components/Counter.jsx
@@ -3,28 +3,19 @@ import { useState } from "react";
 const Counter = () => {
    const [count, setCount] = useState(0);
 
-   const [color, setColor] = useState("green");
-
    const handleChange = (value) => {
       if (value === 2) {
          setCount(count * value);
-         setColor("green");
       }
       else {
          setCount(count + value);
-         if ((count + value) % 2 === 0) {
-            setColor("green");
-         }
-         else {
-            setColor("red")
-         }
       }
    }
 
    return (
       <div>
          <h1>Counter</h1>
-         <h2 className={color}>{count}</h2>
+         <h2 className={count % 2 === 0 ? "green" : "red"}>{count}</h2>
          <button onClick={() => handleChange(1)}>Add</button>
          <button onClick={() => handleChange(-1)}>Subtract</button>
          <button onClick={() => handleChange(2)}>Double</button>
