a867ae5fa677a81f122714b9300341908064624b
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-13 14:02:33 +0530

    react-todo

--- react-todo/src/components/Todo.jsx
+++ react-todo/src/components/Todo.jsx
@@ -4,20 +4,36 @@ import TodoList from './TodoList';
 
 export default function Todo() {
    const [todos, setTodos] = useState([]);
-   const [checked, setChecked] = useState(new Array(todos.length).fill(false));
+   // const [doneTodos, setDoneTodos] = useState(todos.filter(item => {
+   //    return item.status === true;
+   // }));
+
+
+   // For data addition
 
    const addItem = (data) => {
       // console.log(data);
       setTodos([...todos, data]);
-      console.log(todos);
+      // console.log(todos);
    }
 
-   const checkFunc = (position) => {
-      checked[position] = !checked[position];
-      console.log(checked);
-      console.log(todos);
-      setChecked(checked);
+
+   // For List deletion
+
+   const deletion = (position) => {
+      todos.splice(position, 1);
+      setTodos([...todos]);
    }
+
+
+   // For checked feature
+
+   const checkFunc = (item) => {
+      item.status = !item.status;
+      // console.log(todos);
+      setTodos([...todos]);
+   }
+
 
    return (
       <div className='todo'>
@@ -26,17 +42,29 @@ export default function Todo() {
          <div className='list'>
             <div className="allT">
                {
-                  todos.map((e, index) => {
-                     return <div className={checked[index] === true ? "itemChecked" : 'item'}><input onChange={() => { checkFunc(index) }} type="checkbox" className="check" /> {e.title}</div>
+                  todos.map((item, index) => {
+                     return <div className={item.status === true ? "itemChecked" : 'item'}>
+                        <input onChange={() => { checkFunc(item) }} type="checkbox" className="check" />
+                        {item.title}
+                        <button className='btn3' onClick={() => { deletion(index) }}>⁃</button>
+                     </div>
                   })
                }
             </div>
             <div className='compT'>
-               <button className='btn2'>Show Completed To-Dos</button>
+               {/* <button className='btn2' onClick={() => { mapping() }}>Show Completed To-Dos</button> */}
                <div className='list2'>
-                  {
-
-                  }
+                  {/* {
+                     function mapping() {
+                        doneTodos.map((item, index) => {
+                           return <div className="itemChecked">
+                              <input checked type="checkbox" className="check" />
+                              {item.title}
+                              <button className='btn3' onClick={() => { deletion(index) }}>⁃</button>
+                           </div>
+                        })
+                     }
+                  } */}
                </div>
             </div>
          </div>
