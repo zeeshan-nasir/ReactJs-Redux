988b91fcbd485b007cd6b8146b531da74a9d0cba
Zeeshan Nasir <zeeeshannasir94@gmail.com>
2022-04-20 14:21:17 +0530

    useref

---
+++ Practice/useref/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Timer from "./components/Timer";
+import Divref from "./components/Divref";
+
+function App() {
+   return (
+      <div className="App">
+         <Timer />
+         {/* <DivRef /> */}
+      </div>
+   );
+}
+
+export default App;
---
+++ Practice/useref/src/components/Divref.jsx
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+import "./divref.css";
+
+
+export default function DivRef() {
+   const topDivRef = useRef(null);
+   const bottomDivRef = useRef(null);
+
+   return (
+      <div>
+         <div
+            className="coral text"
+            ref={topDivRef}
+            onClick={() => {
+               bottomDivRef.current.scrollIntoView({
+                  behavior: "smooth",
+               });
+            }}
+         >
+            Scroll To Bottom
+         </div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div className="purple"></div>
+         <div className="coral"></div>
+         <div
+            className="purple text"
+            ref={bottomDivRef}
+            onClick={() => {
+               topDivRef.current.scrollIntoView({
+                  behavior: "smooth",
+               });
+            }}
+         >
+            Scroll To Top
+         </div>
+      </div>
+   );
+}
